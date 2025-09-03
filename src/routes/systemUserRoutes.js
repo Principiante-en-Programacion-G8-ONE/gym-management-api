36a93439e8a4a844808fd734e1b8209827813726
MRCC-99 <mariocarrizales60@gmail.com>
2025-09-02 21:34:05 -0600

    feat: add superadmin routes for user management

--- src/routes/systemUserRoutes.js
+++ src/routes/systemUserRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { registerSystemUser, loginSystemUser } = require('../controllers/systemUserController');
-const { protect } = require('../middlewares/authMiddleware');
+const {
+    registerSystemUser,
+    loginSystemUser,
+    getPendingUsers,
+    activateUser
+} = require('../controllers/systemUserController');
+const { protect, isSuperAdmin  } = require('../middlewares/authMiddleware');
 
 // Rutas públicas
 router.post('/register', registerSystemUser);
@@ -12,5 +17,8 @@ router.get('/profile', protect, (req, res) => {
     // La información del usuario está en req.user gracias al middleware 'protect'
     res.json(req.user);
 });
+
+router.get('/pending', protect, isSuperAdmin, getPendingUsers);
+router.patch('/:id/activate', protect, isSuperAdmin, activateUser);
 
 module.exports = router;
