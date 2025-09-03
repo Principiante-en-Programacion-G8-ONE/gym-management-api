44797347252ed426603451c1aa431d1083dbd0f6
MRCC-99 <mariocarrizales60@gmail.com>
2025-09-02 21:34:10 -0600

    feat: add isSuperAdmin middleware for authorization

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
  * @param {object} res - Objeto de respuesta de Express.
  * @param {function} next - Función para pasar al siguiente middleware.
  */
-const protect = (req, res, next) => {
+exports.protect = (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
@@ -34,4 +34,18 @@ const protect = (req, res, next) => {
     }
 };
 
-module.exports = { protect };
+/**
+ * @purpose Middleware para verificar si el usuario es un superadministrador.
+ * Debe usarse DESPUÉS del middleware 'protect'.
+ * @param {object} req - Objeto de solicitud de Express.
+ * @param {object} res - Objeto de respuesta de Express.
+ * @param {function} next - Función para pasar al siguiente middleware.
+ */
+exports.isSuperAdmin = (req, res, next) => {
+    // req.user fue establecido por el middleware 'protect'
+    if (req.user && req.user.is_super_admin) {
+        next();
+    } else {
+        res.status(403).json({ message: 'Forbidden: Access is restricted to super administrators' });
+    }
+};
